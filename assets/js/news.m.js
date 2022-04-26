6856f8787e5367b36dab3d49afeafbe55d188b5a
verythings <78595476+verythings@users.noreply.github.com>
2022-04-26 15:54:23 +0900

    Add files via upload

--- assets/js/news.m.js
+++ assets/js/news.m.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("body").click(function () {
+  $("summary").click(function () {
     $("body").animate(
       {
         scrollTop: $(this).offset().top + "px",
@@ -9,27 +9,15 @@ $(document).ready(function () {
   });
 });
 
-// $(document).on('touchstart', function() {
-//   documentClick = true;
-// });
-// $(document).on('touchmove', function() {
-//   documentClick = false;
-// });
-// $(function () {
-//   $(".top").on("click touchend", function () {
-//     $("body").animate({ scrollTop: $(this).offset().top }, 200);
+// window.addEventListener("DOMContentLoaded", function () {
+//   document.querySelectorAll("details").forEach(function (item) {
+//     item.addEventListener("toggle", (event) => {
+//       let toggled = event.target;
+//       if (toggled.attributes.open) {
+//         document.querySelectorAll("details[open]").forEach(function (opened) {
+//           if (toggled != opened) opened.removeAttribute("open");
+//         });
+//       }
+//     });
 //   });
 // });
-
-window.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("details").forEach(function (item) {
-    item.addEventListener("toggle", (event) => {
-      let toggled = event.target;
-      if (toggled.attributes.open) {
-        document.querySelectorAll("details[open]").forEach(function (opened) {
-          if (toggled != opened) opened.removeAttribute("open");
-        });
-      }
-    });
-  });
-});
