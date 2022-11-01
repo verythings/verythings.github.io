224bb6b050a8882bd7ce37d0b0b7fa73d3ece6d3
verythings <78595476+verythings@users.noreply.github.com>
2022-11-01 18:34:57 +0900

    Add files via upload

--- assets/js/video.js
+++ assets/js/video.js
@@ -48,4 +48,21 @@ class VideoResponser {
   }
 }
 
+var autoplayVideoInterval = setInterval("autoplayVideo()", 200);
+
+function autoplayVideo() {
+  var promise = document.querySelector("video").play();
+  if (promise !== undefined) {
+    promise
+      .then(function (_) {
+        // Autoplay started!
+        clearInterval(autoplayVideoInterval);
+      })
+      .catch(function (error) {
+        // Autoplay was prevented.
+        // Show a "Play" button so that user can start playback.
+      });
+  }
+}
+
 new VideoResponser("#intro-video");
